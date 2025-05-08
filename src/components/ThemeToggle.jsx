af0c9375c1e1207d4d700b3f2025c237a03cc2f6
resikrys <resicris@gmail.com>
2025-05-08 10:42:20 +0200

    update themeToggle file

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,9 +1,7 @@
-//New light-dark trial -> desde app.jsx(theme)
+//TOGGLE LIGHT-DARK THEME
 import { useEffect, useState } from 'react';
 import { Sun, Moon } from "lucide-react";
-// import { theme } from '../App'; // importar el objeto de temas
 import { theme } from '../themes/theme.js'; // importar el objeto de temas
-//../images/dino.jpeg
 
 const ThemeToggle = () => {
   const [isDark, setIsDark] = useState(
@@ -35,37 +33,3 @@ const ThemeToggle = () => {
 };
 
 export default ThemeToggle;
-
-
-//First trial light-dark -> not working pero logica esta be
-/*
-import { useEffect, useState } from 'react';
-
-const ThemeToggle = () => {
-  const [isDark, setIsDark] = useState(
-    () => localStorage.getItem('theme') === 'dark' || false
-  );
-
-  useEffect(() => {
-    const html = document.documentElement;
-    if (isDark) {
-      html.classList.add('dark');
-      localStorage.setItem('theme', 'dark');
-    } else {
-      html.classList.remove('dark');
-      localStorage.setItem('theme', 'light');
-    }
-  }, [isDark]);
-
-  return (
-    <button
-      onClick={() => setIsDark(prev => !prev)}
-      className="px-4 py-2 rounded-xl border border-[theme(--primary-color)] text-[theme(--white)] hover:bg-[theme(--primary-color)] hover:text-[theme(--gray-dark)] transition"
-    >
-      {isDark ? '☀️ Modo Claro' : '🌙 Modo Oscuro'}
-    </button>
-  );
-};
-
-export default ThemeToggle;
-*/
