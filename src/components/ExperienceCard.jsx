77ad0dbbc2bfeb46705147425eb2e3590304a8b0
resikrys <resicris@gmail.com>
2025-05-08 12:07:37 +0200

    update experience card

--- src/components/ExperienceCard.jsx
+++ src/components/ExperienceCard.jsx
@@ -1,36 +1,5 @@
-// //Light-darK
-// import React from 'react';
-
-// export default function ExperienceCard({ frontTitle, frontPlace, backDescription }) {
-//   return (
-//     <div className="w-64 h-40 perspective">
-//       <div className="relative w-full h-full transition-transform duration-700 transform-style-preserve-3d group hover:rotate-y-180">
-//         {/* Frente */}
-//         <div className="absolute w-full h-full bg-black text-white rounded-xl shadow-lg flex flex-col items-center justify-center backface-hidden">
-//         <div className='custom-border-box p-2'>
-//           <div className="right-corner" />
-//           <div className="left-corner" />
-//           <h3 className="text-lg font-bold">{frontTitle}</h3>
-//           <p className="text-sm text-[var(--light-gray)]">{frontPlace}</p>
-//           </div>
-//         </div>
-
-//         {/* Reverso */}
-//         <div className="absolute w-full h-full  bg-black text-white rounded-xl shadow-lg flex items-center justify-center rotate-y-180 backface-hidden p-4 text-sm">
-//           <div className='custom-border-box'>
-//           <div className="right-corner" />
-//           <div className="left-corner" />
-//           <p>{backDescription}</p>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-//New mobile tootltip
+//New mobile tootltip - flipcard component
 import React, { useState } from 'react';
-// import { Info } from 'lucide-react';
 import { RefreshCcw } from 'lucide-react';
 
 export default function ExperienceCard({ frontTitle, frontPlace, backDescription, isMobile }) {
@@ -86,28 +55,3 @@ export default function ExperienceCard({ frontTitle, frontPlace, backDescription
     </div>
   );
 }
-
-
-
-
-// src/components/ExperienceCard.jsx
-// import React from 'react';
-
-// export default function ExperienceCard({ frontTitle, frontPlace, backDescription }) {
-//   return (
-//     <div className="w-64 h-40 perspective">
-//       <div className="relative w-full h-full transition-transform duration-700 transform-style-preserve-3d group hover:rotate-y-180">
-//         {/* Frente */}
-//         <div className="absolute w-full h-full bg-white text-gray-800 rounded-xl shadow-lg flex flex-col items-center justify-center backface-hidden">
-//           <h3 className="text-lg font-bold">{frontTitle}</h3>
-//           <p className="text-sm">{frontPlace}</p>
-//         </div>
-
-//         {/* Reverso */}
-//         <div className="absolute w-full h-full bg-violet-700 text-white rounded-xl shadow-lg flex items-center justify-center rotate-y-180 backface-hidden p-4 text-sm">
-//           <p>{backDescription}</p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
