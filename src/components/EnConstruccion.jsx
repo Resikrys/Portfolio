22a8716973c7d60dac4a4db00b3a04105c88d74a
resikrys <resicris@gmail.com>
2025-05-08 12:49:54 +0200

    update project section & underConstruction component files

--- src/components/EnConstruccion.jsx
+++ src/components/EnConstruccion.jsx
@@ -19,16 +19,3 @@ export default function EnConstruccion({ onClose }) {
       </div>
     );
   }
-
-
-//Mockup under construction Page
-// export default function EnConstruccion() {
-//     return (
-// <section id="en-construccion">
-//   <div className="h-screen flex items-center justify-center flex-col text-center bg-[var(--background-color)] text-[var(--dark-gray)] p-8">
-//     <h1 className="text-4xl font-bold mb-4">🚧 En construcción</h1>
-//     <p className="text-lg max-w-xl">Este proyecto está en desarrollo. Pronto podrás explorarlo desde aquí.</p>
-//   </div>
-// </section>
-//     );
-//   }
