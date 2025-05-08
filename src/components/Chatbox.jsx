fffa3d7ea17d19f21348d4b0b7e01e4040909362
resikrys <resicris@gmail.com>
2025-05-08 12:46:04 +0200

    update dino chatbot file

--- src/components/Chatbox.jsx
+++ src/components/Chatbox.jsx
@@ -1,4 +1,4 @@
-//Second try
+//Dino chatBot component
 export default function ChatBotBox({ onSkip }) {
   return (
     <div className="fixed bottom-24 right-6 bg-[var(--sunDark)] text-[var(--darkSun)] p-4 rounded-xl shadow-lg w-72 z-40">
@@ -14,35 +14,3 @@ export default function ChatBotBox({ onSkip }) {
     </div>
   );
 }
-
-
-//First try
-// import { useState } from "react";
-
-// export default function ChatBotBox({ onSkip }) {
-//   const [isVisible, setIsVisible] = useState(true);
-
-//   const handleClose = () => setIsVisible(false);
-
-//   return (
-//     isVisible && (
-//       <div className="fixed bottom-10 right-10 p-4 bg-gray-800 text-white rounded-lg shadow-lg">
-//         <p>Para descargar el currículum, rellena el formulario o pulsa "Omitir".</p>
-//         <div className="flex space-x-2 mt-2">
-//           <button
-//             onClick={onSkip}
-//             className="bg-yellow-400 hover:bg-yellow-500 text-black p-2 rounded"
-//           >
-//             Omitir
-//           </button>
-//           <button
-//             onClick={handleClose}
-//             className="bg-gray-500 hover:bg-gray-600 text-white p-2 rounded"
-//           >
-//             ✖️
-//           </button>
-//         </div>
-//       </div>
-//     )
-//   );
-// }
